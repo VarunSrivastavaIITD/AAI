2fd3d3024e3433be71e75e2ee8d61a30d9fe1991
varun <varunsrivastava.v@gmail.com>
2018-12-25 00:53:11 +0530

    Add font configuration to plotting

--- src/visualization/egg_and_speech.py
+++ src/visualization/egg_and_speech.py
@@ -7,6 +7,17 @@ from utils import minmaxnormalize, detrend
 plt.switch_backend("QT5Agg")
 plt.rc("text", usetex=True)
 plt.rc("font", family="serif")
+SMALL_SIZE = 22
+MEDIUM_SIZE = 24
+BIGGER_SIZE = 28
+
+plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+plt.rc("axes", titlesize=BIGGER_SIZE)  # fontsize of the axes title
+plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 
 def parse():
@@ -16,7 +27,8 @@ def parse():
         "--speech-folder",
         dest="speechfolder",
         type=str,
-        default="/media/varun/Home/varun/CNN/ECNN/GCI/DotModel/data/bdl_clean/test/speech",
+        # default="/media/varun/Home/varun/CNN/ECNN/GCI/DotModel/data/bdl_clean/test/speech",
+        default="/media/varun/Home/varun/CNN/ECNN/perfect_files/speech",
         help="folder containing speech files",
     )
     parser.add_argument(
@@ -24,25 +36,27 @@ def parse():
         "--egg-folder",
         dest="eggfolder",
         type=str,
-        default="/media/varun/Home/varun/CNN/ECNN/GCI/DotModel/data/bdl_clean/test/egg",
+        # default="/media/varun/Home/varun/CNN/ECNN/GCI/DotModel/data/bdl_clean/test/egg",
+        default="/media/varun/Home/varun/CNN/ECNN/perfect_files/egg_detrended",
         help="folder containing egg files",
     )
     parser.add_argument(
         "-rf",
         "--reconstructedegg-folder",
-        default="/media/varun/Home/varun/CNN/ECNN/GCI/DotModel/data/bdl_clean/test/reconstructed_regressed_cos",
+        # default="/media/varun/Home/varun/CNN/ECNN/GCI/DotModel/data/bdl_clean/test/reconstructed_regressed_cos",
+        default="/media/varun/Home/varun/CNN/ECNN/perfect_files/egg_reconstructed",
         type=str,
         dest="geneggfolder",
         help="folder for reconstructed files",
     )
     parser.add_argument(
-        "-d", "--detrend", type=bool, default=True, help="detrend ground truth egg"
+        "-d", "--detrend", type=bool, default=False, help="detrend ground truth egg"
     )
     parser.add_argument(
         "-f",
         "--file",
         dest="file",
-        default="arctic_a0014.npy",
+        default="arctic_a0015.npy",
         type=str,
         help="File to visualize",
     )
@@ -67,18 +81,29 @@ def main():
     estimated_egg = np.load(estimated_egg_path)
     estimated_egg = minmaxnormalize(estimated_egg)
 
+    region = slice(11000, 12000)
+
     plt.figure()
+    fig = plt.gcf()
     ax = plt.subplot(211)
-    plt.plot(speech, "k", label="Speech Waveform")
+    plt.plot(speech[region], "k", label="Speech Waveform")
     plt.ylabel("Amplitude")
 
     plt.subplot(212, sharex=ax)
-    plt.plot(true_egg, "k", label="Ground Truth EGG")
-    plt.plot(estimated_egg, "b", label="Estimated EGG")
+    plt.plot(true_egg[region], "k", label="Ground Truth EGG")
+    plt.plot(estimated_egg[region], "b", label="Estimated EGG")
     plt.xlabel("Sample Number")
     plt.ylabel("Amplitude")
 
+    plt.subplots_adjust(
+        top=0.962, bottom=0.087, left=0.057, right=0.981, hspace=0.212, wspace=0.2
+    )
+
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
+
     plt.show()
+    # fig.savefig("images/speech_egg.png")
 
 
 if __name__ == "__main__":
--- src/visualization/speechfft.py
+++ src/visualization/speechfft.py
@@ -9,6 +9,17 @@ from utils import minmaxnormalize, detrend, smooth
 plt.switch_backend("QT5Agg")
 plt.rc("text", usetex=True)
 plt.rc("font", family="serif")
+SMALL_SIZE = 22
+MEDIUM_SIZE = 24
+BIGGER_SIZE = 28
+
+plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+plt.rc("axes", titlesize=BIGGER_SIZE)  # fontsize of the axes title
+plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 
 def parse():
@@ -18,7 +29,7 @@ def parse():
         "--speech-folder",
         dest="speechfolder",
         type=str,
-        default="../data/bdl_clean/test/speech",
+        default="../../data/bdl_clean/test/speech",
         help="folder containing speech files",
     )
     parser.add_argument(
@@ -26,13 +37,13 @@ def parse():
         "--egg-folder",
         dest="eggfolder",
         type=str,
-        default="../data/bdl_clean/test/egg",
+        default="../../data/bdl_clean/test/egg",
         help="folder containing egg files",
     )
     parser.add_argument(
         "-rf",
         "--reconstructedegg-folder",
-        default="../data/bdl_clean/test/reconstructed_regressed_cos",
+        default="../../data/bdl_clean/test/reconstructed_regressed_cos",
         type=str,
         dest="geneggfolder",
         help="folder for reconstructed files",
@@ -70,11 +81,12 @@ def main():
     estimated_egg = np.load(estimated_egg_path)
     estimated_egg = minmaxnormalize(estimated_egg)
 
-    srange = slice(0, speech.shape[0])
+    # srange = slice(0, speech.shape[0])
     srange = slice(10700, 15400)
     speech = speech[srange]
 
     plt.figure()
+    fig = plt.gcf()
     plt.subplot(211)
     plt.title("Speech")
     plt.plot(speech, "k", label="Speech Waveform")
@@ -94,12 +106,13 @@ def main():
     plt.ylabel("PSD")
 
     plt.subplots_adjust(
-        top=0.962, bottom=0.061, left=0.041, right=0.99, hspace=0.204, wspace=0.2
+        top=0.926, bottom=0.117, left=0.078, right=0.981, hspace=0.476, wspace=0.2
     )
     mng = plt.get_current_fig_manager()
     mng.window.showMaximized()
 
     plt.show()
+    fig.savefig("images/fft.png")
 
 
 if __name__ == "__main__":
